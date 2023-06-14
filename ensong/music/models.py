0cd3183a79b957b86ffc76b491811839f06e592e
Luke <lukewhrit@pm.me>
2023-06-13 21:58:41 -0400

    fix migrations

--- ensong/music/models.py
+++ ensong/music/models.py
@@ -5,17 +5,19 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.translation import gettext_lazy as _
 
 class Artist(models.Model):
+    mbid = models.UUIDField(_("MusicBrainz ID"), primary_key=True, blank=False, max_length=36)
+
     name = models.CharField(_("artist name"), max_length=255)
-    mbid = models.UUIDField(_("MusicBrainz ID"), blank=True, max_length=36)
     image = models.ImageField(_("image of artist"))
 
 class Genre(models.Model):
     name = models.CharField(_("genre name"), max_length=255)
 
 class Album(models.Model):
+    mbid = models.UUIDField(_("MusicBrainz ID"), primary_key=True, blank=False, max_length=36)
+
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     name = models.CharField(_("Album name"), max_length=255)
-    mbid = models.UUIDField(_("MusicBrainz ID"), max_length=36, primary_key=True, default=None)
     release_date = models.DateField(_("album release date"))
     genre = models.ManyToManyField(Genre)
 
