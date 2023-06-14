733f09115d7b489fd6fef24dcbf1f08b4eae20e6
Luke <lukewhrit@pm.me>
2023-06-14 14:57:15 -0400

    album library page

--- ensong/music/views.py
+++ ensong/music/views.py
@@ -1,10 +1,17 @@
-from typing import Any, Dict
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, ListView
 from .models import Album
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Album
     template_name = "music/home.html"
+    context_object_name = "albums"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["music_page"] = "link-secondary"
+
+        return context
 
 
 class AlbumDetailView(DetailView):
